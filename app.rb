a59a60e11b7ece833adf550c8cfb8147fda004e6
herecomesjaycee <jaycee.cheong04@gmail.com>
2017-01-17 16:30:19 +0000

    Completing #19

--- app.rb
+++ app.rb
@@ -9,16 +9,16 @@ class Battle < Sinatra::Base
   end
 
   post '/names' do
-    params[:player_one_name] << session[:player_one_name].inspect
-    params[:player_two_name] << session[:player_two_name].inspect
+    session[:player_one_name] = params[:player_one_name] 
+    session[:player_two_name] = params[:player_two_name]  
 
     redirect '/play'
   end
 
   get '/play' do
-    params[:player_one_name]
-    params[:player_two_name]
-    erb(:play)
+   @player_one_name = session[:player_one_name] 
+   @player_two_name = session[:player_two_name]
+   erb(:play)
   end
 
   # start the server if ruby file executed directly
