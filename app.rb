f9f6b5e545eb62fb247db594acb8e45f9f56fce1
Matthew Perry <thesedatedprince@gmail.com>
2017-01-17 17:29:53 +0000

    Completed challenge 22

--- app.rb
+++ app.rb
@@ -9,18 +9,24 @@ class Battle < Sinatra::Base
   end
 
   post '/names' do
-    session[:player_one_name] = params[:player_one_name] 
-    session[:player_two_name] = params[:player_two_name]  
+    session[:player_one_name] = params[:player_one_name]
+    session[:player_two_name] = params[:player_two_name]
 
     redirect '/play'
   end
 
   get '/play' do
-   @player_one_name = session[:player_one_name] 
+   @player_one_name = session[:player_one_name]
    @player_two_name = session[:player_two_name]
    @player_one_HP = 100
    @player_two_HP = 100
    erb(:play)
+  end
+
+  get '/attack' do
+   @player_one_name = session[:player_one_name]
+   @player_two_name = session[:player_two_name]
+   erb(:attack)
   end
 
   # start the server if ruby file executed directly
--- spec/features/feature_spec.rb
+++ spec/features/feature_spec.rb
@@ -8,8 +8,16 @@ RSpec.feature "Name form", :type => :feature do
 end
 
 RSpec.feature "Hit points", :type => :feature do
-	scenario "Seeing hit points" do 
+	scenario "Seeing hit points" do
 	sign_in_and_play
 	expect(page).to have_content ('Bill: 100 HP')
+	end
+end
+
+RSpec.feature "Attack", :type => :feature do
+	scenario "Attack player two" do
+		sign_in_and_play
+		click_link('Attack')
+		expect(page).to have_content ('John attacked Bill!')
 	end
 end
