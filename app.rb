fd21d72aa08b33131fc0a07c91b434fc6f616e0a
Matthew Perry <thesedatedprince@gmail.com>
2017-01-17 14:45:00 +0000

    Updated Challenge 18 with feature spec

--- app.rb
+++ app.rb
@@ -2,7 +2,12 @@ require 'sinatra/base'
 
 class Battle < Sinatra::Base
   get '/' do
-    'Hello Battle!'
+    erb(:index)
+  end
+
+  post '/names' do
+    @names = params[:name]
+    erb(:play)
   end
 
   # start the server if ruby file executed directly
--- spec/features/feature_spec.rb
+++ spec/features/feature_spec.rb
@@ -1,7 +1,8 @@
-require 'app'
-require 'capybara'
+require 'capybara/rspec'
+require 'capybara/dsl'
+require 'capybara/rack_test'
 
-Rspec.feature "Name form", :type => :feature do
+RSpec.feature "Name form", :type => :feature do
 	scenario "User enters a name and submit" do
 		fill_in('Name', with: 'John')
 		click_button 'Submit'
