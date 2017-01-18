df25c183b8d2daa1183fc91caacd29057a4fbd20
olwend <olwend@gmail.com>
2017-01-18 15:34:37 +0000

    18. Entering players

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -1,6 +1,9 @@
 feature 'Enter names' do
   scenario 'submitting names' do
-    
+    visit('/')
+    fill_in :player_1_name, with: 'Fox'
+    fill_in :player_2_name, with: 'Badger'
+    click_button 'Submit'
+    expect(page).to have_content "Fox vs. Badger"
   end
-
 end
