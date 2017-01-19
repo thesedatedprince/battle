83af4e03f01432c01463373cb5be0eaa212303c1
olwend <olwend@gmail.com>
2017-01-19 17:57:08 +0000

    26.Additional tests

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,10 +2,24 @@ require './lib/game'
 require './lib/player'
 
 describe Game do
+  subject(:game) { Game.new(player1,player2) }
   let(:player1) { double('player1', :points => 100) }
   let(:player2) { double('player2', :points => 100, :points_damage => 90) }
-  subject(:game) { Game.new(player1,player2) }
-  it 'start new a game with two players' do
+
+  describe '#player_1' do
+    it 'retrieves the first player' do
+      expect(game.player_1).to eq player1
+    end
+  end
+
+  describe '#player_2' do
+    it 'retrieves the second player' do
+      expect(game.player_2).to eq player2
+    end
+  end
+
+
+  it 'starts a new game with two players' do
     expect(Game).to respond_to(:new).with(2).arguments
   end
 
